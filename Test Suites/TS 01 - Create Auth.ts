4b07f57c5dc915f541ea3a01b992216828d42a3d
aziz_ <azizadrian9@gmail.com>
2024-02-13 15:32:59 +0700

    Create Test Suit Case for POST Create Authentication

---
+++ Test Suites/TS 01 - Create Auth.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS 01 - Create Auth</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8fb30f7c-7f1a-483b-a992-a14b68a74077</testSuiteGuid>
+   <testCaseLink>
+      <guid>1e13dd97-6c50-4ce0-a463-d8e65bdd0f75</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/POST Create Authentication</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
